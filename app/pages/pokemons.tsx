b780f539b7cff59625f5470c92689be85552fcac
Jakub Boguski <boguskijakub@gmail.com>
2024-10-28 11:40:51 +0100

    feat: update pokemons page

--- app/pages/pokemons.tsx
+++ app/pages/pokemons.tsx
@@ -1,68 +1,39 @@
+import React from "react";
+import { ActivityIndicator } from "react-native";
+
 import {
-  DefaultError,
-  InfiniteData,
-  useInfiniteQuery
-} from "@tanstack/react-query"
-import React from "react"
-import { ActivityIndicator, FlatList, StyleSheet } from "react-native"
-import { useSafeAreaInsets } from "react-native-safe-area-context"
+  PageContainer,
+  PokemonCard,
+  PokemonsFooter,
+} from "@/components";
+import { List } from "@/components/list";
+import { usePokemons } from "@/hooks";
 
-import { PokemonService } from "../../src/services"
-import { Klfgjhsoigbhb, PokemonCard, PokemonsFooter } from "../../src/components"
-import { Pokemon } from "../../src/models"
-
-const PAGE_SIZE = 20
-
-export default function Page() {
-  const insets = useSafeAreaInsets()
-
+export default function PokemonsPage() {
   const { data, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } =
-    useInfiniteQuery<
-      Pokemon,
-      DefaultError,
-      InfiniteData<Pokemon, number>,
-      string[],
-      number
-    >({
-      queryKey: ['pokemons'],
-      queryFn: ({ pageParam }) => PokemonService.getPokemons({
-        limit: PAGE_SIZE,
-        offset: pageParam
-      }),
-      initialPageParam: 0,
-      getNextPageParam: (_, allPages) => allPages.length * PAGE_SIZE
-    })
+    usePokemons();
 
   return (
-    <Klfgjhsoigbhb title="Pokemons">
-      {isLoading && <ActivityIndicator />}
-      {!isLoading && (
-        <FlatList
-          data={data?.pages.flat()}
-          contentContainerStyle={[styles.contentContainerStyle, {
-            paddingBottom: insets.bottom
-          }]}
-          renderItem={
-            ({ item, index }) => (
-              <PokemonCard item={item} isFirst={index === 0} />
-            )
-          }
-          ListFooterComponent={
-            <PokemonsFooter
-              isHidden={!hasNextPage}
-              onPress={fetchNextPage}
-              isLoading={isFetchingNextPage}
-            />
-          }
-        />
-      )}
-    </Klfgjhsoigbhb>
-  )
+    <PageContainer title="Pokemons">
+        {isLoading && <ActivityIndicator />}
+        {!isLoading && data && (
+          <List
+            data={data.pages.flat()}
+            itemExtractor={(item) => ({
+              title: item.name,
+              subtitle: item.url,
+            })}
+            CardComponent={PokemonCard}
+            ListFooterComponent={
+              <PokemonsFooter
+                isHidden={!hasNextPage}
+                onPress={fetchNextPage}
+                isLoading={isFetchingNextPage}
+              />
+            }
+            scrollEnabled={true}
+          />
+        )}
+    </PageContainer>
+  );
 }
-
-
-const styles = StyleSheet.create({
-  contentContainerStyle: {
-    padding: 16,
-  }
-})
